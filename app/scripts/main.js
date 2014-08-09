7b9fadd399814423ca13debdfb7d1cb37036f0ee
Nikita Popov <losnikitos@yandex-team.ru>
2014-08-09 19:31:00 +0400

    Email support (email.js not in repo, because it contains my gmail password)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,31 @@
-$(function() {
+$(function () {
     $('.menu .item').click(function (e) {
         console.log('show tab', this);
         e.preventDefault();
         $(this).tab('show')
     });
+
+
+    $("form").submit(function (e) {
+        e.preventDefault();
+        var $this = $(this);
+        $.post(
+            $this.attr("action"),
+            $this.serialize(),
+            function (data) {
+                alert('Thanks! We will get back to you soon');
+                $('.modal').modal('hide');
+            },
+            "json"
+        ).done(function () {
+                alert('Thanks! We will get back to you soon');
+                $('.modal').modal('hide');
+            }).fail(function () {
+                alert('Thanks! We will get back to you soon');
+                $('.modal').modal('hide');
+            })
+    });
 });
+
+
 
--- server/app.js
+++ server/app.js
@@ -1,11 +1,32 @@
-var express = require('express');
-var server = express();
+var express = require('express'),
+    server = express(),
+    email = require('./email'),
+    fs =  require('fs');
+
 
 server.configure(function(){
+    server.post('/submit', express.bodyParser(), mail);
     server.use('/', express.static(__dirname + '/../static'));
     server.use(express.static(__dirname + '/../static'));
-//    server.use(express.static(__dirname + '/public'));
 });
 
-console.log(process.env.PORT);
 server.listen(process.env.PORT || 80);
+
+function mail(req, res) {
+    console.log('got mail', req.body);
+
+    if(!req.body)
+    {
+        res.send(200);
+        return
+    }
+
+    var sender = req.body.email;
+    var message = req.body.message;
+
+    email.send(sender, message);
+
+    fs.appendFile('emails.txt', new Date().toString() + ' ' + sender + ': ' + message + '\n');
+
+    res.send(200);
+}
